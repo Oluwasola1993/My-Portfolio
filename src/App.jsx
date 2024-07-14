596aad24c095c6e930dfd878ca0fd4d35dc18d5f
Souljaboii <Ojooluwasola7@gmail.com>
2024-07-14 18:34:39 +0100

    Completed the Skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import Hero from "./Components/Hero";
+import Skills from "./Components/Skills";
 
 const App = () => {
   return (
     <div>
       <div>
-      <Navbar />
-      <div id="Hero">
-        <Hero />
+        <Navbar />
+        <div id="Hero">
+          <Hero />
+        </div>
+        <div id="About">
+          <About />
+        </div>
+        <div id="Skills">
+          <Skills />
+        </div>
       </div>
-      <div id="About">
-        <About />
-      </div>
-    </div>
     </div>
   );
 };
---
+++ src/Components/Skills.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+import "./Skills.css";
+
+const Skills = () => {
+  return (
+    <>
+      <div
+        id="Skills"
+        className="section bg-dark bg-gradient shadow-lg text-center text-light p-5"
+      >
+        <h1 className="hidden display-3">My SKills</h1>
+        <h6 className="hidden display-6 px-lg-5 fs-6">
+          I utilize my creativity and technical expertise to solve problems with
+          attention to details, ensuring effective communication and fostering
+          adaptability and team work, while maintaining a keen understanding
+          of user experience and aesthetics.
+        </h6>
+      </div>
+      <div className="skills bg-dark d-grid align-content-center mb-5 ">
+        <div className="wrapper pb-5">
+          <div className="marquee text-white">
+            <div className="marquee__group">
+              <span className="skill-icons--html hidden">
+                <h6 className="">HTML 5</h6>
+              </span>
+              <span className="skill-icons--css hidden">
+                <h6 className="">CSS 3</h6>
+              </span>
+              <span className="skill-icons--javascript hidden">
+                <h6 className="">JAVASCRIPT</h6>
+              </span>
+              <span className="skill-icons--react-dark hidden">
+                <h6 className="">REACT</h6>
+              </span>
+              <span className="skill-icons--vite-dark hidden">
+                <h6 className="">VITE</h6>
+              </span>
+              <span className="skill-icons--bootstrap hidden">
+                <h6 className="">BOOTSTRAP</h6>
+              </span>
+              <span className="skill-icons--tailwindcss-dark hidden">
+                <h6 className="">TAILWIND</h6>
+              </span>
+              <span className="skill-icons--github-dark hidden">
+                <h6 className="">GITHUB</h6>
+              </span>
+              <span className="logos--nodejs hidden">
+                <h6 className="">NODE JS</h6>
+              </span>
+              <span className="skill-icons--figma-dark hidden">
+                <h6 className="">FIGMA</h6>
+              </span>
+            </div>
+
+            <div aria-hidden="true" className="marquee__group">
+              <span className="skill-icons--html hidden">
+                <h6 className="">HTML 5</h6>
+              </span>
+              <span className="skill-icons--css hidden">
+                <h6 className="">CSS 3</h6>
+              </span>
+              <span className="skill-icons--javascript hidden">
+                <h6 className="">JAVASCRIPT</h6>
+              </span>
+              <span className="skill-icons--react-dark hidden">
+                <h6 className="">REACT</h6>
+              </span>
+              <span className="skill-icons--vite-dark hidden">
+                <h6 className="">VITE</h6>
+              </span>
+              <span className="skill-icons--bootstrap hidden">
+                <h6 className="">BOOTSTRAP</h6>
+              </span>
+              <span className="skill-icons--tailwindcss-dark hidden">
+                <h6 className="">TAILWIND</h6>
+              </span>
+              <span className="skill-icons--github-dark hidden">
+                <h6 className="">GITHUB</h6>
+              </span>
+              <span className="logos--nodejs hidden">
+                <h6 className="">NODE JS</h6>
+              </span>
+              <span className="skill-icons--figma-dark hidden">
+                <h6 className="">FIGMA</h6>
+              </span>
+            </div>
+          </div>
+
+          <div className="marquee marquee--reverse">
+            <div className="marquee__group">
+              <span className="skill-icons--html hidden">
+                <h6 className="">HTML 5</h6>
+              </span>
+              <span className="skill-icons--css hidden">
+                <h6 className="">CSS 3</h6>
+              </span>
+              <span className="skill-icons--javascript hidden`">
+                <h6 className="">JAVASCRIPT</h6>
+              </span>
+              <span className="skill-icons--react-dark hidden">
+                <h6 className="">REACT</h6>
+              </span>
+              <span className="skill-icons--vite-dark hidden">
+                <h6 className="">VITE</h6>
+              </span>
+              <span className="skill-icons--bootstrap hidden">
+                <h6 className="">BOOTSTRAP</h6>
+              </span>
+              <span className="skill-icons--tailwindcss-dark hidden">
+                <h6 className="">TAILWIND</h6>
+              </span>
+              <span className="skill-icons--github-dark hidden">
+                <h6 className="">GITHUB</h6>
+              </span>
+              <span className="logos--nodejs hidden">
+                <h6 className="">NODE JS</h6>
+              </span>
+              <span className="skill-icons--figma-dark hidden">
+                <h6 className="">FIGMA</h6>
+              </span>
+            </div>
+
+            <div aria-hidden="true" className="marquee__group">
+              <span className="skill-icons--html hidden">
+                <h6 className="">HTML 5</h6>
+              </span>
+              <span className="skill-icons--css hidden">
+                <h6 className="">CSS 3</h6>
+              </span>
+              <span className="skill-icons--javascript hidden">
+                <h6 className="">JAVASCRIPT</h6>
+              </span>
+              <span className="skill-icons--react-dark hidden">
+                <h6 className="">REACT</h6>
+              </span>
+              <span className="skill-icons--vite-dark hidden">
+                <h6 className="">VITE</h6>
+              </span>
+              <span className="skill-icons--bootstrap hidden">
+                <h6 className="">BOOTSTRAP</h6>
+              </span>
+              <span className="skill-icons--tailwindcss-dark hidden">
+                <h6 className="">TAILWIND</h6>
+              </span>
+              <span className="skill-icons--github-dark hidden">
+                <h6 className="">GITHUB</h6>
+              </span>
+              <span className="logos--nodejs hidden">
+                <h6 className="">NODE JS</h6>
+              </span>
+              <span className="skill-icons--figma-dark hidden">
+                <h6 className="">FIGMA</h6>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
